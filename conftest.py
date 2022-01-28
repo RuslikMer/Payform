9593b81fcb9c3cfc81d951cd31d209108c29624e
merikanovr <SlevinKelevra_1994>
2022-01-28 15:35:15 +0300

    доработка структуры

--- conftest.py
+++ conftest.py
@@ -1,20 +1,30 @@
+import sys
 import pytest
 from selenium import webdriver
 
 
+sys.path.append('pages')
+sys.dont_write_bytecode = True
+
+
+def pytest_addoption(parser):
+    parser.addoption('--url', action='store', default='https://testingqa.payform.ru/',
+                     help='specify the base URL to test against')
+    parser.addoption('--driver', action='store', default='chrome', help='chrome or firefox')
+
+
 @pytest.fixture(scope="session")
 def driver(request):
-    # driver = request.config.getoption('--driver')
-    # if driver == 'firefox' or driver == 'ff':
-    #    driver = webdriver.Firefox()
-    # elif driver == 'chrome':
-    #    driver = webdriver.Chrome()
-    # else:
-    #    raise ValueError('invalid driver name: ' + driver)
-    # driver.base_url = request.config.getoption('--url') or 'https://testingqa.payform.ru/'
+    driver = request.config.getoption('--driver')
+    if driver == 'firefox' or driver == 'ff':
+        driver = webdriver.Firefox()
+    elif driver == 'chrome':
+        driver = webdriver.Chrome()
+    else:
+        raise ValueError('invalid driver name: ' + driver)
 
-    driver = webdriver.Chrome(executable_path="chromedriver.exe")
+    # driver = webdriver.Chrome(executable_path="chromedriver.exe")
     driver.set_window_size(1920, 1080)
-    driver.base_url = 'https://testingqa.payform.ru/'
+    driver.base_url = request.config.getoption('--url') or 'https://testingqa.payform.ru/'
     yield driver
     driver.quit()
--- tests/payform_test.py
+++ tests/payform_test.py
@@ -2,23 +2,23 @@ import pytest
 from pages.payform import Payform
 
 
-@pytest.fixture(scope="module", autouse=True)
-def setup(driver):
-    global payform
-    payform = Payform(driver)
-    payform.go_to_site()
+class TestSuit:
+    @pytest.fixture(scope="module", autouse=True)
+    def setup(self, driver):
+        global payform
+        payform = Payform(driver)
+        payform.go_to_site()
 
+    def test_fill_payform(self):
+        payform.page_has_loaded()
+        payform.set_order_number('тест')
+        payform.set_phone('9991112233')
+        payform.set_amount('100')
+        payform.press_buy()
 
-def test_fill_payform():
-    payform.page_has_loaded()
-    payform.set_order_number('тест')
-    payform.set_phone('9991112233')
-    payform.set_amount('100')
-    payform.press_buy()
-
-def test_fill_payform_v():
-    payform.page_has_loaded()
-    payform.set_order_number('тест')
-    payform.set_phone('9991112233')
-    payform.set_amount('100')
-    payform.press_buy()
+    def test_fill_payform_v(self):
+        payform.page_has_loaded()
+        payform.set_order_number('тест')
+        payform.set_phone('9991112233')
+        payform.set_amount('100')
+        payform.press_buy()
