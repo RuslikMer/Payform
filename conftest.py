6e309d2c16d7e7fcb7d46c5ad5f8d6797017d18d
merikanovr <SlevinKelevra_1994>
2022-01-28 19:15:33 +0300

    доработка структуры

--- conftest.py
+++ conftest.py
@@ -1,7 +1,10 @@
 import sys
+import time
+from datetime import datetime
+import allure
 import pytest
 from selenium import webdriver
-
+from allure_commons.types import AttachmentType
 
 sys.path.append('pages')
 sys.dont_write_bytecode = True
@@ -28,3 +31,32 @@ def driver(request):
     driver.base_url = request.config.getoption('--url') or 'https://testingqa.payform.ru/'
     yield driver
     driver.quit()
+
+
+# настройка хука, чтобы проверить, прошел ли тест
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    rep = outcome.get_result()
+
+    setattr(item, "rep_" + rep.when, rep)
+
+
+# проверка прошел ли тест
+@pytest.fixture(scope="function", autouse=True)
+def test_failed_check(request):
+    yield
+    if request.node.rep_setup.failed:
+        print("настройка теста не удалась!", request.node.nodeid)
+    elif request.node.rep_setup.passed:
+        if request.node.rep_call.failed:
+            driver = request.node.funcargs['selenium_driver']
+            take_screenshot(driver, request.node.nodeid)
+            print("не удалось выполнить тест", request.node.nodeid)
+
+
+# скриншот с названием теста, датой и временем
+def take_screenshot(driver, nodeid):
+    time.sleep(1)
+    file_name = f'{nodeid}_{datetime.today().strftime("%Y-%m-%d_%H:%M")}.png'.replace("/", "_").replace("::", "__")
+    allure.attach(driver.save_screenshot(file_name), name="Screenshot", attachment_type=AttachmentType.PNG)
