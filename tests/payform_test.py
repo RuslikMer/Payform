99eca8f6abd39230f3673b1fdc3d1b91fce4e234
merikanovr <SlevinKelevra_1994>
2022-04-05 18:48:03 +0300

    fix7

--- tests/payform_test.py
+++ tests/payform_test.py
@@ -18,7 +18,7 @@ class TestPayform(unittest.TestCase):
         chrome_options.add_experimental_option("prefs", {"profile.managed_default_content_settings.images": 2})
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-setuid-sandbox")
-        chrome_options.add_argument("--remote-debugging-port=9222")
+        #chrome_options.add_argument("--remote-debugging-port=9222")
         chrome_options.add_argument("--disable-dev-shm-using")
         chrome_options.add_argument("--disable-extensions")
         chrome_options.add_argument("--disable-gpu")
